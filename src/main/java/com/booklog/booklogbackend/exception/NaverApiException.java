216e7b031906e822c5259b69d17685b0195dc602
OriNong <min000922@gmail.com>
2025-05-02 14:07:07 +0900

    네이버 도서 검색 시 CustomException

---
+++ src/main/java/com/booklog/booklogbackend/exception/NaverApiException.java
@@ -0,0 +1,11 @@
+package com.booklog.booklogbackend.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public class NaverApiException extends RuntimeException {
+    public NaverApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
