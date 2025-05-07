ca22e13a17af9d116831b94348eaef536fddd7fd
OriNong <min000922@gmail.com>
2025-05-07 10:43:36 +0900

    사용자 프로필 응답

---
+++ src/main/java/com/booklog/booklogbackend/Model/response/UserProfileResponse.java
@@ -0,0 +1,17 @@
+package com.booklog.booklogbackend.Model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfileResponse {
+    private String email; // 사용자 이메일
+    private String nickname; // 사용자 닉네임
+    private LocalDateTime createAt; // 가입 일자
+    private LocalDateTime updatedAt; // 회원 정보 수정 일자
+}
