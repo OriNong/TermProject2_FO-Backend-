efd36344319b5b5d366c0f551efa2a8d74b0b3f7
OriNong <min000922@gmail.com>
2025-05-07 10:43:17 +0900

    User Mapper

--- src/main/java/com/booklog/booklogbackend/mapper/UserMapper.java
+++ src/main/java/com/booklog/booklogbackend/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.booklog.booklogbackend.mapper;
 
+import com.booklog.booklogbackend.Model.response.UserProfileResponse;
 import com.booklog.booklogbackend.Model.vo.UserVO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -7,6 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     void insertUser(UserVO userVO);
     UserVO findByEmail(String email);
+    UserProfileResponse findByUserId(Long userId);
     UserVO findByTokenId(String tokenId);
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickname);
