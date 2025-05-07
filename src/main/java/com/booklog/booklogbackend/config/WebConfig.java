fad5d6280a383e3e226f612fb51022646af9a5bf
OriNong <min000922@gmail.com>
2025-05-07 10:44:08 +0900

    CORS 설정

--- src/main/java/com/booklog/booklogbackend/config/WebConfig.java
+++ src/main/java/com/booklog/booklogbackend/config/WebConfig.java
@@ -10,8 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://localhost:8080") // 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+                .allowedHeaders("*") // 또는 "Authorization", "Content-Type"
+                .exposedHeaders("Authorization") // 응답에서 Authorization 헤더도 볼 수 있게
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
