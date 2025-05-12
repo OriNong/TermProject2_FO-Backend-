0d7cc73574bb7d7438044aaaad5fd1036e94efb3
OriNong <min000922@gmail.com>
2025-05-12 10:38:30 +0900

    인증 예외 세분화(auth/login, auth/refresh)

--- src/main/java/com/booklog/booklogbackend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/booklog/booklogbackend/config/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -29,7 +28,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String requestURI = request.getRequestURI();
-        if (requestURI.startsWith("/api/auth/") || requestURI.equals("/api/test/sample")) {
+        if (requestURI.startsWith("/api/auth/login") || requestURI.equals("/api/test/sample") || requestURI.startsWith("/api/auth/refresh")) {
             filterChain.doFilter(request, response);
             return;
         }
