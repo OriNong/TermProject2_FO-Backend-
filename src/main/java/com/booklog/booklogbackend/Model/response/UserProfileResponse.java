521fe679e363dde74d1d31ce5aa4c088ab988566
OriNong <min000922@gmail.com>
2025-05-12 10:25:15 +0900

    /auth/user에서 userId 반환 추가

--- src/main/java/com/booklog/booklogbackend/Model/response/UserProfileResponse.java
+++ src/main/java/com/booklog/booklogbackend/Model/response/UserProfileResponse.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserProfileResponse {
+    private Long userId;
     private String email; // 사용자 이메일
     private String nickname; // 사용자 닉네임
     private LocalDateTime createAt; // 가입 일자
