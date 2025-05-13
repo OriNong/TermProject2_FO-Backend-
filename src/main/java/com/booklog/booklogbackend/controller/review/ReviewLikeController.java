b6d144ce3464f1cb40271e3dd7170a66937ccc1f
OriNong <min000922@gmail.com>
2025-05-13 14:53:57 +0900

    리뷰 좋아요 기능

--- src/main/java/com/booklog/booklogbackend/controller/review/ReviewLikeController.java
+++ src/main/java/com/booklog/booklogbackend/controller/review/ReviewLikeController.java
@@ -20,7 +20,7 @@ public class ReviewLikeController {
     /**
      * 리뷰에 좋아요 시 리뷰에 대한 사용자의
      * 이전 좋아요 상태( [이미 좋아요 한 리뷰] or [아직 좋아요 하지 않은 리뷰] )에 따라
-     * [좋아요 등록] or [좋아요 취소] 처리
+     * [좋아요 등록] or [좋아요 취소] 처리, 처리 후 book_reivew.likes_count 컬럼 값 업데이트
      * @param userId : 로그인 사용자 고유 id
      * @param reviewId : 좋아요를 누른 리뷰 고유 id
      * @return : 좋아요(등록 or 취소) 상태 + 처리 이후 총 좋아요 수
